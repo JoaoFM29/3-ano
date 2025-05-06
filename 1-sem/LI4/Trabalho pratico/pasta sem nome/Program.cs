1163569ede345bccf6e117e60817b0f2887f8e0d
João Magalhães <113180567+JoaoFM29@users.noreply.github.com>
2025-05-07 00:00:32 +0100

    Add files via upload

---
+++ 1-sem/LI4/Trabalho pratico/pasta sem nome/Program.cs
@@ -0,0 +1,18 @@
+using BirdBoxFull.Client;
+using BirdBoxFull.Client.Services.ServicoProduto;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IServicoProduto, ServicoProduto>();
+builder.Services.AddScoped<IServicoUtilizador, ServicoUtilizador>();
+builder.Services.AddScoped<IServicoLicitacao, ServicoLicitacao>();
+builder.Services.AddScoped<IServicoEncomenda, ServicoEncomenda>();
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
